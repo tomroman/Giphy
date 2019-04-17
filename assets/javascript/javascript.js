aef94e7372bb880f463cf820be2d44139edcfc02
Thomas Roman <thomasroman32@yahoo.com>
2019-04-16 23:58:02 -0500

    creating function that allows the stopping and starting of gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,76 +2,97 @@
 var memes = ["salt bae", "dab", "spongebob"];
 console.log(memes)
 
-function renderButtons() { 
+function renderButtons() {
 
-    $("#memes-view").empty(); 
+  $("#button-bar").empty();
 
-       for (var i = 0; i < memes.length; i++) {
+  for (var i = 0; i < memes.length; i++) {
 
-        var a = $("<button>");
+    var newButton = $("<button>").text(memes[i]);
 
-        a.addClass("meme-btn");
-        a.attr("data-meme", memes[i]);
-        a.text(memes[i]);
-        $("#memes-view").append(a);
+    newButton.addClass("btn btn-secondary meme");
+    newButton.attr("data-meme", memes[i]);
+    
+    $("#button-bar").append(newButton);
 
+  }
+}
+
+$("#add-meme").on("click", function (event) {
+  event.preventDefault();
+  var userInput = $("#meme-input").val().trim();
+  
+  if (!memes.includes(userInput) && userInput !== "") {
+    memes.push(userInput);
+  }
+  renderButtons();
+  $("#meme-input").val("");
+
+});
+
+
+function displayMeme() {
+
+  $("#meme-view").empty();
+
+  var meme = $(this).attr("data-meme");
+
+
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    meme + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+    .then(function (response) {
+
+
+      var results = response.data;
+
+
+      for (var i = 0; i < results.length; i++) {
+        var memeDiv = $("<div class='meme-div'>");
+
+
+        memeDiv.html(`
+        <p>Rating: ${results[i].rating}</p>
+        <img src='${results[i].images.fixed_height_still.url}' data-still='${results[i].images.fixed_height_still.url}' data-animate='${results[i].images.fixed_height.url}' data-state='still' class='gif'>
+        `);       
+
+
+          $("#meme-view").prepend(memeDiv);
+        
       }
-     }
+    });
+};
 
-     $("#add-meme").on("click", function(event) { 
-        event.preventDefault();
-        var meme = $("#meme-input").val().trim();
-        memes.push(meme);
-        renderButtons();
+$(document).on("click", ".gif", function () {
 
-     });
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+    
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 
 
-      function displayMeme() {
-           
-            var meme = $(this).attr("data-meme");
-            console.log(this);
-      
-            
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-              meme + "&api_key=dc6zaTOxFJmzC&limit=10";
-      
-            
-            $.ajax({
-              url: queryURL,
-              method: "GET"
-            })
-              
-              .then(function(response) {
-                console.log(queryURL);
-      
-                console.log(response);
-                
-                var results = response.data;
-      
-              
-                for (var i = 0; i < results.length; i++) {
 
-                  if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-      
-                
-                  var memeDiv = $("<div>");
-                  var p = $("<p>").text("Rating: " + results[i].rating);
-                  var memeImage = $("<img>");                 
-                  memeImage.attr("src", results[i].images.fixed_height.url);                     
-                  memeDiv.append(p);
-                  memeDiv.append(memeImage);   
-                  
-                  $("#gifs-appear-here").prepend(memeDiv);
-                }
-              }
-            });
-          };
+})
 
-          $(document).on("click", ".meme-btn", displayMeme);
 
-          renderButtons();
+$(document).on("click", ".meme-btn", displayMeme);
 
+renderButtons();
+
+        //   create a function that can clear the existing gifs
 // create if statement that can code the starting and stoping of a gif. 
 
 
